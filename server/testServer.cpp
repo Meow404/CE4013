95888ffb369f13744295f3369f7f0ef972cd7137
Meow404 <stephenthomasfelix@yahoo.com>
2021-03-27 13:36:07 +0800

    server implementation

--- server/testServer.cpp
+++ server/testServer.cpp
@@ -1,57 +1,12 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <iostream>
-#define PORT     8080
-#define MAXLINE 1024
+#include "udpServer.h"
 
 using namespace std;
 
 // Driver code
 int main() {
-    int sockfd;
-    char buffer[MAXLINE];
-    char *hello = "Hello from server";
-    struct sockaddr_in servaddr, cliaddr;
-      
-    // Creating socket file descriptor
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-      
-    // memset(&servaddr, 0, sizeof(servaddr));
-    // memset(&cliaddr, 0, sizeof(cliaddr));
-      
-    // Filling server information
-    servaddr.sin_family    = AF_INET; // IPv4
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(PORT);
-      
-    // Bind the socket with the server address
-    if ( bind(sockfd, (const struct sockaddr *)&servaddr, 
-            sizeof(servaddr)) < 0 )
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-      
-    int len, n;
-  
-    len = sizeof(cliaddr);  //len is value/resuslt
-  
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-                MSG_WAITALL, ( struct sockaddr *) &cliaddr,
-                (socklen_t*)&len);
-    buffer[n] = '\0';
-    printf("Client : %s\n", buffer);
-    sendto(sockfd, (const char *)hello, strlen(hello), 
-        MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
-            len);
-    printf("Hello message sent.\n"); 
-      
+    char* message;
+    udpServer s(8080);
+    message = s.recieveMessage();
+    s.sendMessage(message, sizeof(message));
     return 0;
 }
---
+++ server/udpServer.cpp
@@ -0,0 +1,45 @@
+#include "udpServer.h"
+
+using namespace std;
+
+udpServer::udpServer(int port)
+{
+    // Creating socket file descriptor
+    if ((socketFd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("SOCKET CREATION FAILED");
+        exit(EXIT_FAILURE);
+    }
+
+    // Filling server information
+    serverAddress.sin_family = AF_INET; // IPv4
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(port);
+
+    // Bind the socket with the server address
+    if (bind(socketFd, (const struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    clientLen = sizeof(clientAddress);
+}
+char *udpServer::recieveMessage()
+{
+    char buffer[1024];
+    int n = recvfrom(socketFd, (char *)buffer, 1024, 0, (struct sockaddr *)&clientAddress, (socklen_t *)&clientLen);
+
+    cout << "Client : " << getClientIP() << " Message : " << buffer;
+    return buffer;
+}
+void udpServer::sendMessage(const char *buffer, int bufferSize)
+{
+    sendto(socketFd, buffer, bufferSize, 0, (const struct sockaddr *)&clientAddress, clientLen);
+}
+string udpServer::getClientIP()
+{
+    char ip[16];
+    inet_ntop(AF_INET, &clientAddress, ip, 16);
+    return (string)ip;
+}
