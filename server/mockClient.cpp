1b3c1c5aaa8bbede2440df61d9d70d05be96846c
Meow404 <stephenthomasfelix@yahoo.com>
2021-03-27 18:30:22 +0800

    check

--- server/mockClient.cpp
+++ server/mockClient.cpp
@@ -21,7 +21,7 @@ int main()
     marshalInt(1, hello);
     marshalInt(15, &hello[4]);
     marshalString("HI FROM CLIENT!", &hello[8]);
-    cout << hello;
+    cout << hello << endl;
     // Creating socket file descriptor
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
