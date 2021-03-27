7148456b56373b54e69980e699e2b2793cec22e3
Meow404 <stephenthomasfelix@yahoo.com>
2021-03-27 18:11:54 +0800

    fixes

--- server/testServer.cpp
+++ server/testServer.cpp
@@ -6,17 +6,17 @@ using namespace std;
 
 // Driver code
 int main() {
-    char message[1024], buffer[1024];
+    char buffer[1024];
     udpServer server(8080);
     server.recieveMessage(buffer, 1024);
     int id = unmarshalInt(buffer);
-    int size = unmarshalInt(buffer[4]);
-    string message = unmarshalString(buffer[8], size);
+    int size = unmarshalInt(&buffer[4]);
+    string message = unmarshalString(&buffer[8], size);
     std::cout << "Id : " << id << " message : " << message <<std::endl;
 
     marshalInt(1, buffer);
-    marshalInt(15, buffer[4]);
-    marshalString("HI FROM SERVER!", buffer[8]);
-    server.sendMessage(message, 1024);
+    marshalInt(15, &buffer[4]);
+    marshalString("HI FROM SERVER!", &buffer[8]);
+    server.sendMessage(buffer, 1024);
     return 0;
 }
